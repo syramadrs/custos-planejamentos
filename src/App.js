7b4f86e847335037651855dca988e1088042f5d4
Damarys <damarys@verion.ind.br>
2023-09-11 21:00:39 -0300

    Imp.Container,Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+
+import Container from "./components/layout/Container";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <div className="App">
-        <p>
-          Costs
-        </p>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
+
+      </Switch>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App;
