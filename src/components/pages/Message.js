1ae9bbad0f0afd7bdd80196ba1057c208f3653dc
Damarys <damarys@verion.ind.br>
2023-09-26 20:26:29 -0300

    Imp. buscando dados do db.json e criação de card

--- src/components/pages/Message.js
+++
@@ -1,7 +0,0 @@
-import styles from './Message.module.css'
-
-function Message({type, msg}) {
-    return <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
-}
-
-export default Message
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,11 +1,57 @@
-import Message from "./Message"
+import { useLocation } from "react-router-dom";
 
-function Projects() {
-    return
-    <div>
-        <h1>Projects</h1>
-        <Message msg="Alguma mensagem"/>
+import { useEffect, useState } from "react";
+
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+
+import styles from "./Projects.module.css";
+
+function Project() {
+  const [projects, setProjects] = useState([]);
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto"></LinkButton>
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+            id={project.id}
+            name={project.name}
+            budget={project.budget}
+            category={project.category.name}
+            key={project.id}
+            />
+          ))}
+      </Container>
     </div>
+  );
 }
 
-export default Projects
+export default Project;
